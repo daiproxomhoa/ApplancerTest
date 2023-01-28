8ea7552eb48213163e83ed048bbbebf167b60a9a
vu tien dai <dai.vu@medring.co.jp>
2023-01-28 18:58:21 +0700

    virtual image

--- src/page/DetailPage.js
+++ src/page/DetailPage.js
@@ -3,6 +3,9 @@ import { Link, useParams } from "react-router-dom";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Virtual, Navigation, Pagination } from "swiper";
+
+SwiperCore.use([Virtual, Navigation, Pagination]);
 
 const DetailPage = () => {
   const { id } = useParams();
@@ -24,16 +27,50 @@ const DetailPage = () => {
   }, [getData]);
 
   if (loading) {
-    return "Loading...";
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <svg
+          class="animate-spin -ml-1 mr-3 h-10 w-10 text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            class="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="4"
+          ></circle>
+          <path
+            class="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
+      </div>
+    );
   }
   if (!data) {
-    return "Product not found!";
+    return (
+      <div className="flex justify-center items-center h-screen">
+        Product not found!
+      </div>
+    );
   }
 
   return (
     <>
       <div>
-        <Swiper>
+        <Swiper
+          centeredSlides={true}
+          pagination={{
+            type: "bullets",
+          }}
+          navigation={true}
+          virtual
+        >
           {data?.images?.map((item, index) => {
             return (
               <SwiperSlide key={index} virtualIndex={index}>
