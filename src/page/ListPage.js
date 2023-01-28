cf1a12266948832d019809199233af1ee5e128e5
vu tien dai <dai.vu@medring.co.jp>
2023-01-28 21:13:58 +0700

    okie

--- src/page/ListPage.js
+++ src/page/ListPage.js
@@ -1,14 +1,24 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import useDebounce from "../hook/useDebounce";
 
 const ListPage = () => {
+  const ref = useRef(null);
   const [term, setTerm] = useState("");
   const debouncedValue = useDebounce(term, 300);
   const [data, setData] = useState([]);
   const [page, setPage] = useState(0);
-  const [total, setTotal] = useState(0);
+  const [total, setTotal] = useState(9);
   const [loading, setLoading] = useState(true);
+
+  const observer = useRef(
+    new IntersectionObserver((entries) => {
+      const first = entries[0];
+      if (first.isIntersecting) {
+        setPage((no) => no + 1);
+      }
+    })
+  );
 
   const getData = useCallback(async () => {
     setLoading(true);
@@ -18,40 +28,37 @@ const ListPage = () => {
       }&limit=9&q=${debouncedValue}`
     );
     const json = await res.json();
-    setData((old) => [...old, ...json.products]);
-    setTotal(json.total);
+    setData((old) => (page ? [...old, ...json.products] : json.products));
+    if (!page) {
+      setTotal(json.total);
+    }
     setLoading(false);
   }, [debouncedValue, page]);
 
   useEffect(() => {
-    getData();
-  }, [getData]);
-
-  const onScroll = useCallback(
-    (e) => {
-      if (
-        window.innerHeight + window.pageYOffset >= document.body.offsetHeight &&
-        !loading &&
-        (page + 1) * 9 < total
-      ) {
-        setPage((old) => old + 1);
-      }
-    },
-    [loading, page, total]
-  );
+    if (page * 9 < total) {
+      getData();
+    }
+  }, [getData, page, total]);
 
   useEffect(() => {
     setPage(0);
-    setTotal(0);
-    setData([]);
   }, [debouncedValue]);
 
   useEffect(() => {
-    window.addEventListener("scroll", onScroll);
+    const currentElement = ref.current;
+    const currentObserver = observer.current;
+
+    if (currentElement) {
+      currentObserver.observe(currentElement);
+    }
+
     return () => {
-      window.removeEventListener("scroll", onScroll);
+      if (currentElement) {
+        currentObserver.unobserve(currentElement);
+      }
     };
-  }, [onScroll]);
+  }, []);
 
   return (
     <div className="container py-4">
@@ -119,6 +126,31 @@ const ListPage = () => {
       {!loading && data.length === 0 && (
         <p className="text-xl text-center mx-auto">No data found!</p>
       )}
+      {loading && (
+        <div className="flex justify-center">
+          <svg
+            className="animate-spin -ml-1 mr-3 h-10 w-10 text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        </div>
+      )}
+      <div ref={ref} />
     </div>
   );
 };
